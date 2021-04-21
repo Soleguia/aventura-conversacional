e18c7d2d0d608d19023157f21cc7644f8a6d1548
Josu <soleguia@gmail.com>
2021-04-21 07:59:05 +0200

    Adding History

---
+++ src/components/HistoryComponent.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const HistoryComponent = (props) => {
+    // const [history, setHistory] = useState([])
+    console.log({props})
+    return (
+        <h2>Conversación</h2>
+
+    )
+}
+
+export default HistoryComponent
--- src/views/StoryView.js
+++ src/views/StoryView.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
+import HistoryComponent from '../components/HistoryComponent'
 import { StoryComponent } from '../components/StoryComponent'
 
 export const StoryView = () => {
     const [fragments, setFragments] = useState([])
     const [currentFragment, setCurrentFragment] = useState(0)
     const [showFragment, setShowFragment] = useState(false)
+    const [history, setHistory] = useState({})
 
     useEffect( () => {
         fetch('./data/fragments.json')
@@ -24,10 +26,17 @@ export const StoryView = () => {
         return fragments.find( fragment => fragment.id === current )
     }
     
+    const nextFragment = (id) => {
+        setCurrentFragment(id)
+        setHistory(showFragment)
+    }
     return (
         <div className="story">
             {
-                showFragment ? <StoryComponent fragment={showFragment} sendData={setCurrentFragment}></StoryComponent> : ''
+                history ? <HistoryComponent history={history}></HistoryComponent> : ''
+            }
+            {
+                showFragment ? <StoryComponent fragment={showFragment} sendData={nextFragment}></StoryComponent> : ''
             }
         </div>
     )
