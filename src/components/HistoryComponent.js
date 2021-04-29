fa3f7c3a248aaecf6de0ea6c51a64dc24598ecca
Josu <soleguia@gmail.com>
2021-04-29 07:44:12 +0200

    History auto scroll

--- src/components/HistoryComponent.js
+++ src/components/HistoryComponent.js
@@ -1,16 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import { StoryComponent } from './StoryComponent'
 
 // import styles from '../css/story-component.module.css'
 
 const HistoryComponent = memo((props) => {
+    let messagesEnd = useRef();
+
+    const scrollToBottom = () => {
+        messagesEnd.current.scrollTop = messagesEnd.current.scrollHeight;
+    }
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [props.history])
+
     return (
         <React.Fragment>
             <div className="storyHistory">
-                <div className="historyFragments">
+                <div className="historyFragments" ref={messagesEnd}>
                 {
                     props.history.map( h => {
-                        console.log({h});
                         return (
                             <React.Fragment key={h.id}>
                                 <StoryComponent storyFragment={h}></StoryComponent>
